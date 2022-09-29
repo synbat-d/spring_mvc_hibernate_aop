0dc8ff6a2e8a8307776a2e972472187a298e9921
Symbat Dulatuly <synbat1999@gmail.com>
2022-09-28 17:11:03 -0700

    ready code for uploading to github

---
+++ src/main/java/com/spring/mvc/dao/EmployeeDAO.java
@@ -0,0 +1,16 @@
+package com.spring.mvc.dao;
+
+import com.spring.mvc.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
---
+++ src/main/java/com/spring/mvc/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.spring.mvc.service;
+
+import com.spring.mvc.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
